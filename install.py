347c602743417935c26356f1ec923b3de9e3a61f
popoimm <birdswww@gmail.com>
2017-08-29 19:55:22 +0800

    Update install.py
    tst

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ import getopt
 import filecmp
 
 openresty_pkg_url = 'https://openresty.org/download/openresty-1.11.2.4.tar.gz'
-openresty_pkg = 'openresty-1.9.15.1.tar.gz'
+openresty_pkg = 'openresty-*.tar.gz'
 
 work_path = os.getcwd()
 
